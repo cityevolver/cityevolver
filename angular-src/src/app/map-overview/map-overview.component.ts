d4096efd173321d028250341d3a2679702ca912a
Manu <manu.aiumi@gmail.com>
2019-05-04 17:25:30 +0200

    build

--- angular-src/src/app/map-overview/map-overview.component.ts
+++ angular-src/src/app/map-overview/map-overview.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-overview.component.scss']
 })
 export class MapOverviewComponent implements OnInit {
-  private title = 'City evolver APP!';
+  title = 'City evolver APP!';
 
   constructor() { }
 
