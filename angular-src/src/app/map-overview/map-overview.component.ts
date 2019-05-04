6f1031e298904660180732dc4d814a27ac873286
Manu <manu.aiumi@gmail.com>
2019-05-04 23:52:42 +0200

    Leaflet added

--- angular-src/src/app/map-overview/map-overview.component.ts
+++ angular-src/src/app/map-overview/map-overview.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DataApiService, IssueDetail, IssueType} from '../data-api.service';
 
+// declare variable for maps (leaflet)
+declare let L;
+
 @Component({
   selector: 'app-map-overview',
   templateUrl: './map-overview.component.html',
@@ -20,6 +23,11 @@ export class MapOverviewComponent implements OnInit {
 
   ngOnInit() {
     this.getAllIssues();
+
+
+
+
+    // var mymap = L.map('mapid').setView([51.505, -0.09], 13);
   }
 
 
@@ -29,9 +37,20 @@ export class MapOverviewComponent implements OnInit {
 
     this.dataApi.getAllIssues().subscribe(response => {
       this.allIssuesData = response;
+      setTimeout(() =>
+        this.loadMap(), 0);
     }, error => {
       this.allIssuesData = null;
       this.issuesLoadError = true;
     });
+  }
+
+  private loadMap() {
+    // const map = L.map('mapid').setView([51.505, -0.09], 13);
+    const map = L.map('mapid').setView([50.092, 14.414], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
   }
 }
